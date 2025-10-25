b5e26482c8f2cb0c91dd8f737d69aa4c4c963c6f
Sabplpz <sabrinalpz1994@gmail.com>
2025-10-25 13:09:40 -0400

    small change public to template

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
     const port = 3000;
 
     // Tell Express to serve the static files from the 'public' directory
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'templates')));
 
     app.listen(port, () => {
         console.log(`Server is running at http://localhost:${port}`);
