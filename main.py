d13d7eaddeebe25ea66e146e6de25e1dd328f113
therealfajita <fontejon.jrl08@gmail.vom>
2025-10-25 15:13:47 -0400

    Skeleton budget calculation

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/features/financial_planning.html', methods=['POST'])
+def calculate_budget(salary):
+    return {
+        "Housing": round(salary * 0.30, 2),
+        "Food": round(salary * 0.15, 2),
+        "Savings": round(salary * 0.20, 2),
+        "Entertainment": round(salary * 0.10, 2),
+        "Miscellaneous": round(salary * 0.25, 2)
+    }
+@app.route('/features/financial_planning.html', methods=['GET'])
+def financial_planning():
+    return render_template('features/financial_planning.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
