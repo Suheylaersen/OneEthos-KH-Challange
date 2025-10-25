9206cf90d5e24cc0a89ac1d454c0d6bafd3a872a
Sabplpz <sabrinalpz1994@gmail.com>
2025-10-25 14:34:48 -0400

    Revert "Adding node files"
    This reverts commit 2c21b627ba31f7ee3a58c06c0ba25403e5dee6e2.

--- financial_agent/agent.py
+++ financial_agent/agent.py
@@ -1,9 +1,8 @@
 from google.adk.agents.llm_agent import Agent
 
-financial_agent = Agent(
+root_agent = Agent(
     model='gemini-2.5-flash',
-    name='financial_agent',
-    description='A helpful asistant for personal finance questions',
-    instruction='Answer financial questions with detail and provide plans for the user to achieve goals.',
+    name='root_agent',
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge',
 )
-
